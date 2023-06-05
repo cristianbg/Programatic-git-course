2c1fb0a1018064832082ef46d8bcf8a07a07f892
Cristian Georgescu <cristianbujor@yahoo.com>
2023-06-05 16:00:18 +0200

    Added the jokes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 
+jokes = [
+    "Why did the angry function exceed the callstack size? It got into an Argument with itself",
+    "Whats the object-oriented way to become wealthy? Inheritance"
+]
+
 @app.get("/")
-def hello_world():
-    return "<p>Hello World!</p>"
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
